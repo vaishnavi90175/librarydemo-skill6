7a32123b86263be5dd27c5832ac2cef022121eaf
vaishnavi90175 <2400090175@kluniversity.in>
2026-03-14 19:55:05 +0530

    Commit Success

---
+++ SpringAutowireDemo/src/main/java/com/example/autowireddemo/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.example.autowireddemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.autowireddemo")
+public class AppConfig {
+
+}
---
+++ SpringAutowireDemo/src/main/java/com/example/autowireddemo/main/package-info.java
@@ -0,0 +1,1 @@
+package com.example.autowireddemo.main;
